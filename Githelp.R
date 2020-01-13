bf86f0a7a8cade6c7ee00fc789e9a372b04ac431
Ferris Nowlan <ferrisnowlan@Ferriss-MacBook.local>
2020-01-13 16:48:25 -0500

    new

--- Githelp.R
+++ Githelp.R
@@ -1,3 +1,9 @@
 #Commit
 #Pull
 #Push
+
+#our little git window (top right) shows us things that have changed since our last sync
+#Commit them, leaving a note for your collaborators. Green means added lines, red means deleted lines
+#Then push them to github
+
+print("Hello World")
