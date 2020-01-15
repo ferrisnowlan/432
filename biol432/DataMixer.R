b999cf71f513c213daca74d969142d7dc865ed83
Ferris Nowlan <ferrisnowlan@Ferriss-MacBook.local>
2020-01-15 14:41:15 -0500

    I think I'm done now

--- biol432/DataMixer.R
+++ biol432/DataMixer.R
@@ -4,21 +4,20 @@ library(dplyr)
 
 #2: create Volume column
 #assume Limb Width is the diameter of a perfectly circular leg 
-#end goal: put everything in cm^3
-
+#end goal: calculate Volume in cm^3
 
 #fill Limb.Volume column, correcting Limb.Width units, but ignoring Limb.Length unit issues for now
 for (i in 1:length(MData$unitsW)){
   if(MData$unitsW[i] == "mm"){ 
-    MData$Limb.Volume[i] <- pi*(((MData$Limb.Width[i])*10/2)^2)*MData$Limb.Length[i]
+    MData$Limb.Volume[i] <- pi*(((MData$Limb.Width[i])/10/2)^2)*MData$Limb.Length[i]
   } else{
     MData$Limb.Volume[i] <- pi*(((MData$Limb.Width[i])/2)^2)*MData$Limb.Length[i]
   }
 }
 
-#okay, now correct the Limb.Length unit issue, but dividing final volume by 10 if Limb.Length was in mm
+#okay, now correct the Limb.Length unit issue, by dividing final volume by 10 if Limb.Length was in mm
 for (i in 1:length(MData$unitsL)){
-  if(MData$unitsL[i] == "mm"){ #the 2 is again the 'second factor', mm. Alphabetical.
+  if(MData$unitsL[i] == "mm"){ 
     MData$Limb.Volume[i] <- ((MData$Limb.Volume[i])/10)
   }
 }
@@ -26,4 +25,39 @@ for (i in 1:length(MData$unitsL)){
 #create units column for Limb.Volume
 MData$unitsV[1:10] <- "cm^3"
   
+
+##part III
+library(ggplot2)
+
+#I need to convert Limb.Width and Limb.Length to same units again...
+
+#initialize vector
+Limb.Width.cm <- 1:10
+
+#add values
+for (i in 1:length(MData$unitsL)){
+  if(MData$unitsL[i] == "mm"){
+    Limb.Width.cm[i] <- ((MData$Limb.Width[i])/10) 
+  } else {
+    Limb.Width.cm[i] <- (MData$Limb.Width[i])
+  }
+}
+
+#initialize vector
+Limb.Length.cm <- 1:10
+
+#add values
+for (i in 1:length(MData$unitsL)){
+  if(MData$unitsL[i] == "mm"){ 
+    Limb.Length.cm[i] <- ((MData$Limb.Length[i])/10)
+  } else {
+    Limb.Length.cm[i] <- (MData$Limb.Length[i])
+  }
+}
+
+#plot
+qplot(x=Limb.Width.cm, y= Limb.Length.cm, xlab= "Limb Width (cm)", 
+      ylab= "Limb Length (cm)", main= "Limb Dimension Comparison Across Species",
+      alpha = (I(0.5))) +
+  theme_classic()
   
